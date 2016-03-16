97122cfb9b4c3f3f9279701a1ea3db81e05723b0
Steve Henke <SHenke@paylocity.com>
2016-03-16 11:08:46 -0500

    Initial commit.

---
+++ build.ps1
@@ -0,0 +1,26 @@
+﻿cls
+
+# '[p]sake is the same as 'psake' but $Error is not pulluted
+Remove-Module [p]sake
+
+# this section future-proofs in case multiple side-by-side versions available (not likely)
+# Import-Module ..\packages\psake.*\tools\psake.psm1 <-- this *could* be an issue
+
+# find psake's path
+$psakeModule = (Get-ChildItem (".\packages\psake*\tools\psake.psm1")).FullName | Sort-Object $_ | select -Last 1
+
+#Import-Module $psakeModule
+Import-Module .\Packages\psake.4.5.0\tools\psake.psm1
+
+Invoke-psake -buildFile .\Build\default.ps1 `
+             -taskList Test `
+             -framework 4.5.2 `
+             -properties @{
+               "buildConfiguration" = "Release"
+               "buildPlatform" = "Any CPU"} `
+             -parameters @{"solutionFile"="..\psake.sln"} #TODO Can remove quotes from testMessage (like with solutionFile - what is correct?
+
+Write-Host "Build exit code: $LastExitCode"
+
+#Propogating the exist code so that builds actually fail when there is a problem.
+exit $LastExitCode
